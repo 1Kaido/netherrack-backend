30d9854e2b659c06bfd24e65f96cb45ccfbc3c2f
Shaikh Mohd Zaid Matin <zaidshaikh964862@gmail.com>
2026-09-06 12:50:08 +0530

    Registered sse route

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ CORS(app)
 jwt = JWTManager(app)
 oauth.init_app(app)
 app.register_blueprint(auth_bp)
-
+app.register_blueprint(sse_bp, url_prefix="/api")
 with app.app_context():
     db.create_all()
     
