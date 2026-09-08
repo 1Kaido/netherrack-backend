02a51c4649ad5df182529ff8c8bba6db95fb0e1a
1kaido <zaidshaikh964862@gmail.com>
2026-09-08 13:13:26 +0530

    Added cookies and refresh tokens

--- app.py
+++ app.py
@@ -1,23 +1,14 @@
 from dotenv import load_dotenv
 load_dotenv()
 from datetime import date
+from datetime import timedelta
 import os
 import json
 import queue
 from threading import Thread
-from flask import (
-    Flask,
-    request,
-    jsonify,
-    Response,
-    stream_with_context,
-)
+from flask import (Flask,request,jsonify,Response,stream_with_context)
 from flask_cors import CORS
-from flask_jwt_extended import (
-    JWTManager,
-    jwt_required,
-    get_jwt_identity,
-)
+from flask_jwt_extended import (JWTManager,jwt_required,get_jwt_identity)
 from routes.auth_routes import auth_bp
 from sse.routes import sse_bp
 from extensions import oauth
@@ -28,28 +19,25 @@ from agent import (
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
-    "DATABASE_URL"
-)
-
-app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
-    "pool_pre_ping": True,
-    "pool_recycle": 300,
-}
-
-app.config["JWT_SECRET_KEY"] = os.getenv(
-    "JWT_SECRET_KEY"
-)
-
-app.config["SECRET_KEY"] = os.getenv(
-    "SECRET_KEY"
-)
+#CONFIGS
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {"pool_pre_ping": True,"pool_recycle": 300,}
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config['JWT_TOKEN_LOCATION'] = ['cookies'] 
+app.config['JWT_COOKIE_SECURE'] = True
+app.config['JWT_COOKIE_HTTPONLY'] = True
+app.config['JWT_COOKIE_SAMESITE'] = 'None'
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=15)
+app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
 
 
 db.init_app(app)
-CORS(app)
+CORS(app, supports_credentials=True)
 jwt = JWTManager(app)
 oauth.init_app(app)
+
+#BLUEPRINTS
 app.register_blueprint(auth_bp)
 app.register_blueprint(sse_bp, url_prefix="/api")
 with app.app_context():
@@ -75,15 +63,20 @@ def user_prompt():
             "success": False,
             "error": "No prompt sent"
         }), 400
-        
-        researcher = create_researcher_on_tier(tier)
-        result = researcher(
-        f"""
-        Today's date is {today}.
-        User's task:
-        {prompt}
-        """
-        )
+    
+    today = date.today()
+    researcher = create_researcher_on_tier(tier)
+    result = researcher(
+    f"""
+    Today's date is {today}.
+    User's task:
+    {prompt}
+    """
+    )
+    return jsonify({
+        "success": True,
+        "result": result
+    }), 200
         
 
 if __name__ == "__main__":
--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -1,56 +1,71 @@
 import os
 from flask import Blueprint, redirect, url_for
-from flask_jwt_extended import create_access_token  # pyright: ignore[reportMissingImports]
-
+from flask_jwt_extended import create_access_token  
 from extensions import google
 from models import User, db
+from flask import Flask, jsonify, request
+from flask_jwt_extended import(
+    JWTManager, create_access_token, create_refresh_token,jwt_required,get_jwt_identity,set_refresh_cookies,unset_jwt_cookies
+)
+from datetime import timedelta
+from flask_cors import CORS
+import datetime
+from flask_jwt_extended import set_access_cookies
+from datetime import timedelta
 
-
+app = Flask(__name__)
+CORS(app, supports_credentials=True) 
 auth_bp = Blueprint("auth", __name__)
-
-FRONTEND_URL = os.getenv("FRONTEND_URL", "https://thenetherrack.vercel.app")
-
-
+#FRONTEND_URL = os.getenv("FRONTEND_URL", "https://thenetherrack.vercel.app")
+FRONTEND_URL = os.getenv("FRONTEND_URL", "http://127.0.0.1:5000")
 @auth_bp.route("/auth/google")
 def google_login():
-
     redirect_uri = url_for(
         "auth.google_callback",
         _external=True
     )
-
     return google.authorize_redirect(
         redirect_uri
     )
 
-
 @auth_bp.route("/auth/google/callback")
 def google_callback():
-
     token = google.authorize_access_token()
-
     userinfo = token["userinfo"]
-
     google_id = userinfo["sub"]
     email = userinfo["email"]
     name = userinfo.get("name")
-
     user = User.query.filter_by(
         google_id=google_id
     ).first()
-
     if not user:
-
-        user = User(
-            google_id=google_id,
-            email=email
-        )
-
+        user = User(google_id=google_id,email=email)
         db.session.add(user)
         db.session.commit()
 
-    access_token = create_access_token(
-        identity=str(user.id)
-    )
+        # Create tokens
+        access_token = create_access_token(identity=str(user.id))
+        refresh_token = create_refresh_token(identity=str(user.id))
+        response = jsonify({
+            'message':'login done'
+        })
+        #set cookies
+        set_access_cookies(response,access_token)
+        set_refresh_cookies(response,refresh_token)
+        return response
+    
+    return jsonify({
+        "message":"login done"
+    })
 
-    return redirect(f"{FRONTEND_URL}/callback.html?token={access_token}")
+@app.route('/refresh')
+@jwt_required(refresh=True)
+def refresh_token():
+    user_id = get_jwt_identity()
+    new_access_token = create_access_token(identity=user_id)
+    response = jsonify({
+        "message": "Token refreshed"
+    })
+    set_refresh_cookies(response,new_access_token)
+
+
